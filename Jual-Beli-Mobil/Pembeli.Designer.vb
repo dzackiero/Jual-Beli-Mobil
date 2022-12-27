fec30ade325403f3df488c0c3ed3e0df6618b8e7
izznoiz <naovalmalhiz@gmail.com>
2022-12-27 21:40:29 +0700

    first commit

--- Jual-Beli-Mobil/Pembeli.Designer.vb
+++ Jual-Beli-Mobil/Pembeli.Designer.vb
@@ -22,9 +22,63 @@ Partial Class Pembeli
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Id_Pembeli = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Nik_pembeli = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Nama_pembeli = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Alamat_pembeli = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Id_Pembeli, Me.Nik_pembeli, Me.Nama_pembeli, Me.Alamat_pembeli})
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 12)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowTemplate.Height = 25
+        Me.DataGridView1.Size = New System.Drawing.Size(654, 263)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'Id_Pembeli
+        '
+        Me.Id_Pembeli.HeaderText = "Id Pembeli"
+        Me.Id_Pembeli.Name = "Id_Pembeli"
+        '
+        'Nik_pembeli
+        '
+        Me.Nik_pembeli.HeaderText = "Nik Pembeli"
+        Me.Nik_pembeli.Name = "Nik_pembeli"
+        Me.Nik_pembeli.Width = 140
+        '
+        'Nama_pembeli
+        '
+        Me.Nama_pembeli.HeaderText = "Nama Pembeli"
+        Me.Nama_pembeli.Name = "Nama_pembeli"
+        Me.Nama_pembeli.Width = 180
+        '
+        'Alamat_pembeli
+        '
+        Me.Alamat_pembeli.HeaderText = "Alamat Pembeli"
+        Me.Alamat_pembeli.Name = "Alamat_pembeli"
+        Me.Alamat_pembeli.Width = 190
+        '
+        'Pembeli
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(678, 450)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "Pembeli"
         Me.Text = "Pembeli"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
     End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Id_Pembeli As DataGridViewTextBoxColumn
+    Friend WithEvents Nik_pembeli As DataGridViewTextBoxColumn
+    Friend WithEvents Nama_pembeli As DataGridViewTextBoxColumn
+    Friend WithEvents Alamat_pembeli As DataGridViewTextBoxColumn
 End Class
--- Jual-Beli-Mobil/Pembeli.vb
+++ Jual-Beli-Mobil/Pembeli.vb
@@ -1,3 +1,5 @@
 ﻿Public Class Pembeli
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
 
+    End Sub
 End Class
